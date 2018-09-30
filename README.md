1ac252c9cad66b52eaaf7f28ddd30f5c0298b3c3
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-09-30 15:20:34 +0900

    Update installer

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ Yet Another Salesforce Dataloader
 
 For Windows user with cmd.exe
 ```
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('http://install.freedom-man.com/yasd.ps1'))"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile ^
+  -InputFormat None -ExecutionPolicy Bypass ^
+  -Command "iex ((New-Object System.Net.WebClient).DownloadString('http://install.freedom-man.com/yasd.ps1'))" ^
+  && SET "PATH=%PATH%;%APPDATA%\yasd\bin"
 ```
 For Windows user with cmd.exe
 ```powershell
--- install.ps1
+++ install.ps1
@@ -11,20 +11,23 @@ $archive_file = "${name}-${version}-windows-${arch}.zip"
 $url = "https://github.com/${github_user}/${name}/releases/download/v${version}/${archive_file}"
 $dest_dir = "$Env:APPDATA\${name}"
 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
 wget $url -OutFile "${dest_dir}\tmp.zip"
 Expand-Archive -Path "${dest_dir}\tmp.zip" -DestinationPath "${dest_dir}\tmp" -Force
 if (!(Test-Path -path "${dest_dir}\bin")) {  New-Item "${dest_dir}\bin" -Type Directory }
-mv "${dest_dir}\tmp\${bin_name}" "${dest_dir}\bin\${bin_name}.exe" -Force
+mv "${dest_dir}\tmp\windows-${arch}\${bin_name}" "${dest_dir}\bin\${bin_name}.exe" -Force
 rm "${dest_dir}\tmp.zip" -Force
 rm "${dest_dir}\tmp" -Force -Recurse
 
 # ensure added to path in registry
+$bin_path = "${dest_dir}\bin"
 $reg_path = [Environment]::GetEnvironmentVariable("PATH", "User")
-If ($reg_path -notcontains $dest_dir) {
-  [Environment]::SetEnvironmentVariable("PATH", $reg_path + ";" + $dest_dir, "User")
+If ($reg_path.Contains($bin_path) -eq $false) {
+  [Environment]::SetEnvironmentVariable("PATH", "${reg_path};${bin_path}", "User")
 }
 # ensure added to path for current session
-If ($Env:Path -notcontains $dest_dir) {
-  $Env:Path += ";" + $dest_dir
+If ($($Env:Path).Contains($bin_path) -eq $false) {
+  $Env:Path += ";${bin_path}"
 }
 Write-Host "${name} installed. Run '${name} help' to try it out."
